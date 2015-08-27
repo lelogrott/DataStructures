eec60b9707dbf25c669b59ef17f58995c733d0e7
lelogrott <grott.aurelio@gmail.com>
2015-08-27 16:00:11 -0300

    nova aplicacao feita, menu para usuario adicionado

--- MPE/main.c
+++ MPE/main.c
@@ -1,35 +1,69 @@
-#include "TDA_MpE_PRIV.h"
+#include "TDA_MpE.h"
 
-
-int main()
-{
-
-    pMP p;
-    int consultado;
-    int removido;
-    int novoItem;
-
-    int n=4;
-    int l=3;
-    int tamInfo = sizeof(int);
-
-    cria(&p,n,l,tamInfo);
-
-    novoItem = 5;
-
-    insereNaPilha(p,4, &novoItem);
-
-    consultaTopo(p, 4, &consultado);
+int main(void)
+{ 
+    pMP ptr = NULL; 
+    int nPilha, lPilha;
     
-    printf(">>TOPO: %d\n", consultado);
-
-    removeDaPilha(p, 4, &removido);
+    printf(">Digite o numero de pilhas da MPE:\n");
+    scanf("%d", &nPilha);
+    printf(">Digite o tamanho das pilhas da MPE:\n");
+    scanf("%d", &lPilha);
+    clrscr();
     
-    printf(">>REMOVEU: %d", removido);
+    if(cria(&ptr, nPilha, lPilha, sizeof(int))==SUCESSO)
+        printf(">>criadas %d pilhas de int de tamanho %d\n",nPilha, lPilha);
+    else
+        return FRACASSO;
+    getchar();
+    clrscr();
     
-    consultaTopo(p, 4, &consultado);
+    int opc, novo, consultado, removido;
     
-    destroi(&p);
+    do{
+        printf("1. inserir matricula\n2. remover matricula\n3. consultar topo\n0. sair\n\n> ");
+        scanf("%d", &opc);
+        
+        switch(opc){
+            clrscr();
+            case 1:
+                printf("\nDigite a pilha para insercao:\n> ");
+                scanf("%d", &nPilha);
+                printf("\nDigite a matricula:\n> ");
+                scanf("%d", &novo);
+                if(insereNaPilha(ptr, nPilha, &novo)==SUCESSO)
+                    printf("\nsucesso na insercao!\n");
+                getchar();getchar();
+                clrscr();
+                break;
+            case 2:
+                printf("\nDigite a pilha para remocao:\n> ");
+                scanf("%d", &nPilha);
+                if(removeDaPilha(ptr, nPilha, &removido)==SUCESSO){
+                    printf("\nELEMENTO REMOVIDO:\n-------------------\n%d\n-------------------\n", removido);
+                    printf("\nsucesso na remocao!\n");
+                }
+                getchar();getchar();
+                clrscr();
+                break;
+            case 3:
+                printf("\nDigite a pilha para consulta:\n> ");
+                scanf("%d", &nPilha);
+                if(consultaTopo(ptr, nPilha, &consultado)==SUCESSO){
+                    printf("\nTOPO ATUAL:\n-------------------\n%d\n-------------------\n", consultado);
+                    printf("\nsucesso na consulta!\n");
+                }
+                getchar();getchar();
+                clrscr();
+                break;
+            case 0:
+                destroi(&ptr);
+                break;
+        }
+    }while(opc!=0);
     
-    return 0;
+    
+        return 0;
 }
+
+
